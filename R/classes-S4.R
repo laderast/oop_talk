9734113925e162eef0055ad901c8826b6dfd76ad
Ted Laderas <roshioshi@gmail.com>
2019-04-25 14:47:43 -0700

    update with s4 notebook (still working)

--- R/classes-S4.R
+++ R/classes-S4.R
@@ -1,46 +1,47 @@
 library(dplyr)
 
 #return results
-statResult <- setClass("stat_package_result",
+StatPackageResult <- setClass("StatPackageResult",
                        slots = c(data = "data.frame",
-                                 statistics = "matrix"
+                                 statistics = "data.frame"
                                  ))
 
 #always need to define the generic method first
-setGeneric(name= "get_statistics", 
+setGeneric(name= "statistics", 
            def= function(object) {
              standardGeneric("get_statistics")
              }
            )
 
 #define the function
-setMethod(f="get_statistics",
+setMethod(f="statistics",
           #signature is how the function is called
-          signature = "stat_paackage_result", 
+          signature = signature("StatPackageResult"), 
           definition = function(object){
             return(object@statistics)
           })
 
-setGeneric(name = "get_significant_results", 
-           def = function(object){
+#making a generic with two arguments
+setGeneric(name = "get_significant_results",
+           def = function(object, cutoff){
              standardGeneric("get_significant_results")
              
            } )
 
-#This is our method for our stat_package_result class
+#This is our method for our StatPackageResult class
 setMethod("get_significant_results", 
-          signature = c("stat_package_result", "numeric"),
+          signature = signature(object="StatPackageResult", cutoff="numeric"),
           definition = function(object, cutoff){
             
             if(cutoff < 0 | cutoff > 1){
               stop("Your threshold makes no sense")
             }
             
-            filtered_results <- object@statistics %>%
+            filtered_results <- data.frame(object@statistics) %>%
               filter(pvalue < cutoff)
             
             return(filtered_results)
           })
 
-#checking things about our package
-setValidity()
+#checking things about our class
+#setValidity()
