4ef07351dbbc2c03904f1a3ac47f4eaf57512d8f
Ted Laderas <roshioshi@gmail.com>
2019-04-26 14:00:56 -0700

    update of S4/R6

--- R/classes-R6.R
+++ R/classes-R6.R
@@ -1,19 +1,46 @@
 library(R6)
+library(dplyr)
 
 R6::R6Class(classname = "StatPackageResult",
             public = list(
-              initialize = function(data, statistics){
               
+              #this is what we use to initialize our object
+              initialize = function(data, statistics){
+                self$data <- data
+                self$statistics <- statistics
+                invisible(self)
               },
+              
+              #define our slots here
               data = NULL,
               statistics = NULL,
             
               get_statistics = function(){
+                  return(self$statistics)
+              },
               
+              get_threshold = function(){
+                private$threshold
               },
             
-              get_significant_results = function(){
-              
+              set_threshold = function(threshold){
+                private$threshold <- threshold
               }
-            )
+              
+              get_significant_results = function(){
+                
+                  if(is.null(self$threshold)){
+                    stop("Threshold is not set")
+                  }
+                
+                  filtered_results <- self$statistics %>%
+                    filter(pvalue < threshold)
+                  
+                  filtered_results
+                  
+              }),
+            
+            private=list(
+              threshold=NULL
+              )
             )
--- R/classes-S4.R
+++ R/classes-S4.R
@@ -3,19 +3,18 @@ library(dplyr)
 #S4 object definition
 StatPackageResult <- setClass("StatPackageResult",
                        slots = c(data = "data.frame",
-                                 statistics = "data.frame"),
-                       prototype = c(data=NA, statistics=NA)
+                                 statistics = "data.frame")
                        )
 
 #always need to define the generic method first
-setGeneric(name= "statistics", 
+setGeneric(name= "get_statistics", 
            def= function(object) {
-             standardGeneric("statistics")
+             standardGeneric("get_statistics")
              }
            )
 
 #define the function
-setMethod(f="statistics",
+setMethod(f="get_statistics",
           #signature is how the function is called
           signature = signature("StatPackageResult"), 
           definition = function(object){
@@ -27,7 +26,7 @@ setGeneric(name = "get_significant_results",
            def = function(object, cutoff){
              standardGeneric("get_significant_results")
              
-           } )
+           })
 
 #This is our method for our StatPackageResult class
 setMethod("get_significant_results", 
@@ -45,11 +44,16 @@ setMethod("get_significant_results",
           })
 
 #checking things about our class
-#setValidity()
+check_stats_object <- function(object){
+  #check whether statistics data.frame contains "pvalue"
+  #as column name
+  if(colnames(get_statistics(object)) %in% "pvalue") {TRUE}
+  else{ "statistics slot needs a pvalue column"}
+}
 
-
-
+setValidity(Class = "StatPackageResult", method=check_stats_object)
 
 
 #Inheritance: making a new class from our old class
-
+AnovaResult <- setClass("AnovaResult", 
+                        contains= "StatPackageResult")
