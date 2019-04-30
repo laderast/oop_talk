e9fd00b950c35408a1816b9ce46ace2ef499dfb5
Ted Laderas <roshioshi@gmail.com>
2019-04-30 15:25:39 -0700

    update

--- R/classes-S4.R
+++ R/classes-S4.R
@@ -23,7 +23,7 @@ setMethod(f="get_statistics",
 
 #making a generic with two arguments
 setGeneric(name = "get_significant_results",
-           def = function(object, cutoff){
+           def = function(object, cutoff, ...){
              standardGeneric("get_significant_results")
              
            })
@@ -56,4 +56,15 @@ setValidity(Class = "StatPackageResult", method=check_stats_object)
 
 #Inheritance: making a new class from our old class
 AnovaResult <- setClass("AnovaResult", 
-                        contains= "StatPackageResult")
+                        contains= "StatPackageResult",
+                        slots=c(groups="character"))
+
+#Overriding the get_statistics method
+setMethod("get_statistics",
+          signature = signature(object = "AnovaResult", cutoff="numeric"),
+
+            definition = function(object, cutoff){
+                print(object@groups)
+                callNextMethod()
+              }
+          )
--- README.md
+++ README.md
@@ -5,9 +5,15 @@
 ```
 install.packages(c("R6", "here"))
 ```
-
 ## Getting Started
 
 1. Clone/Download this Repository
 2. Unzip (if necessary) to a folder
 3. Open the `oop_talk.rproj` file in RStudio
+
+## Talk Material
+
+- [Intro Slides](http://laderast.github.io/oop_talk/)
+- [S3 Classes](https://scotttalks.info/rs3/)
+- [S4 Programming Notebook](http://laderast.github.io/oop_talk/S4-objects.nb.html) - Open up `S4-objects.Rmd` (R Notebook)
+- [R6 Programming Notebook](http://laderast.github.io/oop_talk/S4-objects.nb.html) - Open up `R6-objects.Rmd`
